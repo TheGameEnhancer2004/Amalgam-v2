eb04f0aca80313b0dc495d48ff5c0cef14f4ca41
mlemlody <garciaisheree@gmail.com>
2025-12-29 09:13:11 +0100

    Update Controller.cpp

--- Amalgam/src/Features/NavBot/NavEngine/Controllers/Controller.cpp
+++ Amalgam/src/Features/NavBot/NavEngine/Controllers/Controller.cpp
@@ -25,6 +25,35 @@ void CGameObjectiveController::Update()
 		flNextGameTypeRefresh = I::GlobalVars->curtime + 1.0f;
 	}
 
+	auto sMapName = std::string(I::EngineClient->GetLevelName());
+	size_t nLastSlash = sMapName.find_last_of("/\\");
+	if (nLastSlash != std::string::npos)
+		sMapName = sMapName.substr(nLastSlash + 1);
+
+	if (sMapName.find("cppl_") == 0)
+	{
+		F::CPController.Update();
+		F::PLController.Update();
+		return;
+	}
+	if (sMapName.find("vsh_") == 0 || sMapName.find("2koth_") == 0 || sMapName.find("koth_") == 0 || sMapName.find("cp_") == 0 || sMapName.find("tc_") == 0)
+	{
+		F::CPController.Update();
+		return;
+	}
+	if (sMapName.find("pl_") == 0 || sMapName.find("plr_") == 0)
+	{
+		F::PLController.Update();
+		return;
+	}
+	if (sMapName.find("ctf_") == 0 || sMapName.find("sd_") == 0 || sMapName.find("rd_") == 0 || sMapName.find("pd_") == 0)
+	{
+		F::FlagController.Update();
+		if (sMapName.find("sd_doomsday") == 0)
+			F::CPController.Update();
+		return;
+	}
+
 	switch (m_eGameMode)
 	{
 	case TF_GAMETYPE_CTF:
